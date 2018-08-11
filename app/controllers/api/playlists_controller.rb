950777b902fdbb4238c98c33fc27fda35d42dee7
Rowan Littlefield <rowanlittlefield@ymail.com>
2018-08-10 21:03:21 -0400

    Implement method for extracting isrc ids from apple api response

--- app/controllers/api/playlists_controller.rb
+++ app/controllers/api/playlists_controller.rb
@@ -2,17 +2,9 @@ class Api::PlaylistsController < ApplicationController
 
   def shared_track_count
     debugger
-# playlistobject = {tracks: [playlistTrackObject = {track: {external_ids: {isrc: '1'}}}, ...]}
-#tracks is an array of track objects
-#isrc is a string
-    spotify_json_mock = {
-      tracks: [
-        {track: {external_ids: {isrc: '1'}}},
-        {track: {external_ids: {isrc: '2'}}},
-        {track: {external_ids: {isrc: '3'}}}
-      ]
-    }.to_json
+
     spotify_ids = spotify_track_isrc_ids_list(spotify_json_mock)
+    apple_ids = apple_track_isrc_ids_list(apple_json_mock)
 
     debugger
     x = 5
@@ -25,5 +17,86 @@ class Api::PlaylistsController < ApplicationController
     spotify_hash['tracks'].map do |track|
       track["track"]['external_ids']['isrc']
     end
+  end
+
+  def apple_track_isrc_ids_list(apple_json_response)
+    apple_hash = JSON.parse(apple_json_response)
+
+    apple_hash['data'][0]['relationships']['tracks']['data'].map do |track|
+      track['attributes']['isrc']
+    end
+  end
+
+  private
+
+  def spotify_json_mock
+    # playlistobject = {tracks: [playlistTrackObject = {track: {external_ids: {isrc: '1'}}}, ...]}
+    #tracks is an array of track objects
+    #isrc is a string
+
+    {
+      tracks: [
+        {track: {external_ids: {isrc: '1'}}},
+        {track: {external_ids: {isrc: '2'}}},
+        {track: {external_ids: {isrc: '3'}}}
+      ]
+    }.to_json
+  end
+
+  def apple_json_mock
+    {
+      data: [
+        {
+          relationships: {
+            tracks: {
+              data: [
+                {
+                  attributes: {
+                    isrc: "USVI29700014"
+                  }
+                },
+                {
+                  attributes: {
+                    isrc: "USVI20100057"
+                  }
+                },
+                {
+                  attributes: {
+                    isrc: "USVI20100098"
+                  }
+                }
+              ]
+            }
+          }
+        }
+      ]
+    }.to_json
+#     {
+#     "data": [
+#         {
+#             "relationships": {
+#                 "tracks": {
+#                     "data": [
+#                         {
+#                             "attributes": {
+#                                 "isrc": "USVI29700014"
+#                             }
+#                         },
+#                         {
+#                             "attributes": {
+#                                 "isrc": "USVI20100057"
+#                             }
+#                         },
+#                         {
+#                             "attributes": {
+#                                 "isrc": "USVI20100098"
+#                             }
+#                         }
+#                     ]
+#                 }
+#             }
+#         }
+#     ]
+# }
   end
 end
